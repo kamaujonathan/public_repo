91005c707dacb2390d30e059a71c208698739cb5
kamaujonathan <kamaujonathan@gmail.com>
2017-02-07 09:46:16 +0300

    changes made

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,8 +1,8 @@
 n=8
-def check_prime_numbers(n):
+def check_prime_numbers(n): #function to check for prime numbers between 0 and n
         
-        for x in range(0,n+1):
-                if x > 0:
+        for x in range(0,n+1): #the time complexity here can be denoted by 0(n)
+                if x > 0: 
                         for i in range(2,x):
                                 if (x % i) == 0:
                                         break
