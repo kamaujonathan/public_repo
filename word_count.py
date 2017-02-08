4c4c23f33d602bb6442446072b39abbe9c7ecd0f
kamaujonathan <kamaujonathan@gmail.com>
2017-02-08 11:09:53 +0300

    Changes made on the word count code

--- word_count.py
+++ word_count.py
@@ -2,6 +2,8 @@ def words(phrase):
 	result = {}
 	word_count = phrase.split()
 	for word in word_count:
+		if word.isdigit():
+			word = int(word)
 		if word in result:
 			result[word] += 1
 		else:
